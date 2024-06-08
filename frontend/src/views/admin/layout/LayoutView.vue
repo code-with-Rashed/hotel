f284039d26ae172baeca2265774dd65ac2ddd0c1
Rashed <rashedalamj@gmail.com>
2024-06-08 15:07:08 +0600

    create admin layout

---
+++ frontend/src/views/admin/layout/LayoutView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+import SidebarView from './SidebarView.vue'
+const layoutExtand = ref(null)
+</script>
+<template>
+  <div class="wrapper">
+    <SidebarView @layout="(style) => (layoutExtand = style)" />
+    <div class="main p-3" :style="layoutExtand">
+      <slot name="main"></slot>
+    </div>
+  </div>
+</template>
+<style>
+@import '../../../assets/admin_layout.css';
+</style>
---
+++ frontend/src/views/admin/layout/SidebarView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const expand = ref(false)
+
+const emit = defineEmits(['layout'])
+watch(expand, (status) => {
+  if (status) {
+    emit('layout', 'width:calc(100% - 260px)')
+  } else {
+    emit('layout', 'width:calc(100% - 70px)')
+  }
+})
+</script>
+<template>
+  <aside id="sidebar" :class="{ expand }">
+    <div>
+      <button class="toggle-btn" type="button" @click="expand = !expand">
+        <i class="bi bi-ui-radios-grid"></i>
+      </button>
+    </div>
+    <ul class="sidebar-nav">
+      <li class="sidebar-item">
+        <RouterLink :to="{ name: dashboard }" class="sidebar-link">
+          <i class="bi bi-diagram-3 fs-5 me-1"></i>
+          <span>Dashboard</span>
+        </RouterLink>
+      </li>
+    </ul>
+  </aside>
+</template>
