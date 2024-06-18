77ee0daa4dc6f321c35071468e10a039f29a6c27
Rashed <rashedalamj@gmail.com>
2024-06-18 17:58:05 +0600

    Add Facility page

---
+++ frontend/src/views/visitior/FacilitiesView.vue
@@ -0,0 +1,118 @@
+<script setup>
+import LayoutView from './layout/LayoutView.vue'
+</script>
+<template>
+  <LayoutView>
+    <template #page>
+      <div class="my-5 px-4">
+        <h2 class="fw-bold text-center h-fonts">OUR FACILITIES</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi odit saepe et eveniet
+          dolor id aspernatur natus sapiente cum at? <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, pariatur?
+        </p>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG51592.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">wifi</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG96089.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">Telivision</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG87388.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">Ac</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG30732.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">Cleaner</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG77436.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">Hitter</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 mb-5 px-4">
+            <div class="bg-white shadow p-4 rounded border-4 border-top border-dark pop">
+              <div class="d-flex align-items-center mb-2">
+                <img
+                  src="http://localhost/hotel/images/facilities/IMG81256.svg"
+                  alt="facilities"
+                  width="40px"
+                />
+                <h5 class="m-0 ms-3">Micro oven</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus dicta
+                error. Minus repudiandae optio voluptas.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </LayoutView>
+</template>
