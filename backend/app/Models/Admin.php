ba3b6060eb47e3e7553e1edbb34295de088a4034
Rashed <rashedalamj@gmail.com>
2024-07-05 18:49:03 +0600

    show full admin profile photo path help by laravel accessors

--- backend/app/Models/Admin.php
+++ backend/app/Models/Admin.php
@@ -11,4 +11,9 @@ class Admin extends Model
     use HasFactory, HasApiTokens;
 
     protected $hidden = ['password'];
+
+    public function getPhotoAttribute($photo)
+    {
+        return asset('storage') . "/" . $photo;
+    }
 }
