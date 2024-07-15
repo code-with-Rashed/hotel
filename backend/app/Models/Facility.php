2420d048a59ad90a64d41cd06cbcd5bb4574ab1c
Rashed <rashedalamj@gmail.com>
2024-07-15 23:04:09 +0600

    show full room facility image path help by laravel accessors

--- backend/app/Models/Facility.php
+++ backend/app/Models/Facility.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Facility extends Model
 {
     use HasFactory;
+
+    public function getImageAttribute($photo)
+    {
+        return asset('storage') . "/" . $photo;
+    }
 }
