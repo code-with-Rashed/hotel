f5317947b1da907b27aec2dc678b215eddec9136
Rashed <rashedalamj@gmail.com>
2024-07-16 22:53:23 +0600

    show full carousel image path help by laravel accessors

--- backend/app/Models/Carousel.php
+++ backend/app/Models/Carousel.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Carousel extends Model
 {
     use HasFactory;
+
+    public function getImageAttribute($photo)
+    {
+        return asset('storage') . "/" . $photo;
+    }
 }
