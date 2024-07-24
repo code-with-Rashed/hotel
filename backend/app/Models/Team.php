306a9c388e55a3352874a82383e6875d03b1b53f
Rashed <rashedalamj@gmail.com>
2024-07-25 00:07:48 +0600

    show full team photo image path help by laravel accessors

--- backend/app/Models/Team.php
+++ backend/app/Models/Team.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Team extends Model
 {
     use HasFactory;
+
+    public function getPhotoAttribute($photo)
+    {
+        return asset('storage') . "/" . $photo;
+    }
 }
