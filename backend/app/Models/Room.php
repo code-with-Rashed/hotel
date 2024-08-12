5c4ad098696ae8cd722bf7048c2d02c4ac9b1f7e
Rashed <rashedalamj@gmail.com>
2024-08-12 17:04:19 +0600

    handle relationship

--- backend/app/Models/Room.php
+++ backend/app/Models/Room.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Room extends Model
 {
     use HasFactory;
+
+    // assigned room images
+    public function images()
+    {
+        return $this->hasMany(RoomImage::class);
+    }
+
+    // assigned room features
+    public function features()
+    {
+        return $this->belongsToMany(Feature::class, 'room_features');
+    }
+
+    // assigned room facilities
+    public function facilities()
+    {
+        return $this->belongsToMany(Facility::class, 'room_facilities');
+    }
 }
