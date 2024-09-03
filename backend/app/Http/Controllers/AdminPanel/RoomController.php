e09a78d2c1165cd72bb43f46460c6866e683aec2
Rashed <rashedalamj@gmail.com>
2024-09-03 17:49:51 +0600

    change pagination limit

--- backend/app/Http/Controllers/AdminPanel/RoomController.php
+++ backend/app/Http/Controllers/AdminPanel/RoomController.php
@@ -12,7 +12,7 @@ class RoomController extends BaseController
     // show all rooms
     public function index()
     {
-        $results["rooms"] = Room::orderBy('status', 'desc')->paginate(2);
+        $results["rooms"] = Room::orderBy('status', 'desc')->paginate(5);
         return $this->send_response(message: "Rooms data .", results: $results);
     }
 
