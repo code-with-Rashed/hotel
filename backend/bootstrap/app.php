e58164b7571bd1b59438a99a6b8b5dd795a128e5
Rashed <rashedalamj@gmail.com>
2024-09-16 22:46:07 +0600

    setup payment getway
    download sslcommerz payment getway package
    
    setup & config required payment getway option

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -6,13 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: [
+            '/sslcommerz/pay',
+            '/sslcommerz/success',
+            '/sslcommerz/cancel',
+            '/sslcommerz/failure',
+            '/sslcommerz/ipn',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SslCommerzController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::controller(SslCommerzController::class)
+    ->prefix('sslcommerz') // prefix to avoid conflicts
+    ->name('sslc.')
+    ->group(function () {
+        Route::post('pay', 'pay')->name('pay');
+        Route::post('success', 'success')->name('success');
+        Route::post('failure', 'failure')->name('failure');
+        Route::post('cancel', 'cancel')->name('cancel');
+        Route::post('ipn', 'ipn')->name('ipn');
+    });
