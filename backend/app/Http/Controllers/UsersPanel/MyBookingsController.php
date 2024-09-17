1f81dc8c66d8397d3e4dac3160a7bfa85fd7e259
Rashed <rashedalamj@gmail.com>
2024-09-18 02:02:09 +0600

    response by descending order

--- backend/app/Http/Controllers/UsersPanel/MyBookingsController.php
+++ backend/app/Http/Controllers/UsersPanel/MyBookingsController.php
@@ -10,7 +10,7 @@ class MyBookingsController extends BaseController
     // show all booking records for users
     public function index($user_id)
     {
-        $mybookings = BookingOrder::with('booking_details')->where('user_id', $user_id)->get();
+        $mybookings = BookingOrder::with('booking_details')->where('user_id', $user_id)->orderBy('id', 'desc')->get();
         return $this->send_response('Booking Records.', $mybookings);
     }
 }
