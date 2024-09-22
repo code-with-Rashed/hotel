dbc443346675a545dfd897076feb4bf3fb69e89f
Rashed <rashedalamj@gmail.com>
2024-09-23 00:54:01 +0600

    add anothe column

--- backend/database/migrations/2024_09_22_184314_create_rating_reviews_table.php
+++ backend/database/migrations/2024_09_22_184314_create_rating_reviews_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId("user_id")->constrained("users");
             $table->integer("star");
             $table->string("message");
+            $table->boolean("seen")->default(0);
             $table->timestamps();
         });
     }
