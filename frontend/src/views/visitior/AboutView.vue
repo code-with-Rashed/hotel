274cb8745f6797a3bde2e4d9d14431ffef8dabe1
Rashed <rashedalamj@gmail.com>
2024-11-19 10:21:26 +0600

    show achievement record

--- frontend/src/views/visitior/AboutView.vue
+++ frontend/src/views/visitior/AboutView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import LayoutView from './layout/LayoutView.vue'
 import useTeamMemberApi from '@/composables/visitor/teamMemberApi'
+import useAchievementApi from '@/composables/visitor/achievementApi'
 import { ref, onMounted } from 'vue'
 
 const { results, get } = useTeamMemberApi()
+const { results: achievementResults, get: getAchievement } = useAchievementApi()
 
 // show team member records
 const reloader = ref(true)
@@ -13,7 +15,18 @@ const showTeamMembers = async () => {
   reloader.value = true
 }
 
-onMounted(() => showTeamMembers())
+// show achievement records
+const achievementReloader = ref(true)
+const showAchievements = async () => {
+  achievementReloader.value = false
+  await getAchievement()
+  achievementReloader.value = true
+}
+
+onMounted(() => {
+  showAchievements()
+  showTeamMembers()
+})
 </script>
 <template>
   <LayoutView>
@@ -29,67 +42,29 @@ onMounted(() => showTeamMembers())
       </div>
 
       <div class="container">
-        <div class="row align-items-center justify-content-between">
-          <div class="col-lg-6 col-md-5 mb-4 order-lg-0 order-md-0 order-1">
-            <h3 class="mb-3">Lorem ipsum dolor sit amet.</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum repudiandae voluptate
-              id tempora magnam iure laudantium illo ipsam. Reiciendis aliquid consectetur accusamus
-              odio dolorem ipsam vero consequuntur vel assumenda? Commodi, illum repellendus.
-            </p>
-          </div>
-          <div class="col-lg-5 col-md-5 mb-4 order-lg-1 order-md-1 order-0">
-            <img
-              src="http://localhost/hotel/images/about/about.jpg"
-              alt="about image"
-              class="w-100"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
         <div class="row mt-5">
-          <div class="col-lg-3 col-md-6 mb-4 px-4">
-            <div class="bg-white text-center shadow rounded border-top border-4 p-4 box">
-              <img
-                src="http://localhost/hotel/images/about/hotel.svg"
-                alt="about hotel"
-                width="70px"
-              />
-              <h6 class="mt-3">100+ Rooms</h6>
+          <template v-if="achievementReloader">
+            <template v-if="achievementResults.data">
+              <template
+                v-for="achievement in achievementResults.data.achievement"
+                :key="achievement.id"
+              >
+                <div class="col-lg-3 col-md-6 mb-4 px-4">
+                  <div class="bg-white text-center shadow rounded border-top border-4 p-4 box">
+                    <img :src="achievement.photo" alt="about hotel" width="70px" />
+                    <h6 class="mt-3">{{ achievement.achievement }}</h6>
+                  </div>
+                </div>
+              </template>
+            </template>
+          </template>
+          <template v-else>
+            <div class="col-12 px-4 text-center">
+              <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
             </div>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-4 px-4">
-            <div class="bg-white text-center shadow rounded border-top border-4 p-4 box">
-              <img
-                src="http://localhost/hotel/images/about/customers.svg"
-                alt="about customer"
-                width="70px"
-              />
-              <h6 class="mt-3">200+ Customers</h6>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-4 px-4">
-            <div class="bg-white text-center shadow rounded border-top border-4 p-4 box">
-              <img
-                src="http://localhost/hotel/images/about/rating.svg"
-                alt="about ratting"
-                width="70px"
-              />
-              <h6 class="mt-3">100k+ Ratting</h6>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 mb-4 px-4">
-            <div class="bg-white text-center shadow rounded border-top border-4 p-4 box">
-              <img
-                src="http://localhost/hotel/images/about/staff.svg"
-                alt="about staff"
-                width="70px"
-              />
-              <h6 class="mt-3">150+ Staffs</h6>
-            </div>
-          </div>
+          </template>
         </div>
       </div>
       <!-- show team profile start -->
