6755e746a0b624742d1aa0154787d4f22343ca0c
Rashed <rashedalamj@gmail.com>
2025-07-24 07:29:55 +0600

    fetch & show favicon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,18 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+import useFaviconApi from '@/composables/visitor/faviconApi'
+
+// show favicon
+const { results: faviconResults, get: getFavicon } = useFaviconApi()
+const faviconLink = document.querySelector("link[rel='icon']")
+const showFavicon = async () => {
+  await getFavicon()
+  faviconLink.href = faviconResults.value.data.favicon.icon
+}
+onMounted(() => {
+  showFavicon()
+})
 </script>
 <template>
   <RouterView />
